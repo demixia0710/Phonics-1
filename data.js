0bb226eed15ad4dce3bd976be05eaaf54266fc11
demixia0710 <demixia0710@gmail.com>
2026-04-14 15:34:20 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,17 @@
+const families = {
+  at: {
+    words: ["cat", "bat", "hat", "mat"],
+    sentence: "The cat is on the mat.",
+    answer: "cat"
+  },
+  an: {
+    words: ["man", "pan", "fan", "can"],
+    sentence: "The man has a fan.",
+    answer: "man"
+  },
+  ap: {
+    words: ["cap", "map", "nap", "tap"],
+    sentence: "I have a cap.",
+    answer: "cap"
+  }
+};
